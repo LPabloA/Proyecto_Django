a8eca6b0f3f547a3c0692f2ea4dd6290b31f00e5
Pablo Londoño <londono.pablo.andres@gmail.com>
2023-05-24 09:25:08 -0500

    agregado 3 questions y 3 choices para el question pk=1 24-05-23

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
@@ -7,9 +9,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    # funcion para ver si la pregunta fue publicada antes de 1 dia desde el momento de consulta
+
 
 class Choice(models.Model):
-    Question = models.ForeignKey(
+    question = models.ForeignKey(
         Question, on_delete=models.CASCADE)  # Foreign key de question
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
